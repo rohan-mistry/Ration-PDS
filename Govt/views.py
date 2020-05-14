6ef7e3da483451ae95c4ec73d6191c5fa87d0e5e
rohan-mistry <53314296+rohan-mistry@users.noreply.github.com>
2020-05-14 20:01:42 +0530

    added screenshots

--- Govt/views.py
+++ Govt/views.py
@@ -206,18 +206,7 @@ def showPdf(request,shop,month,year):
     pdf.build(elems)
     return response
 
-    # response = HttpResponse(content_type='application/pdf')
-    # response['Content-Disposition'] = 'inline; filename=somefilename.pdf'
 
-    # elements = []
-
-    # doc = SimpleDocTemplate(response, rightMargin=0, leftMargin=6.5 * cm, topMargin=0.3 * cm, bottomMargin=0)
-
-    # # data=[(1,2),(3,4)]
-    # table = Table(data, colWidths=270, rowHeights=79)
-    # elements.append(table)
-    # doc.build(elements) 
-    # return response
     
 
 def searchReport(request):
